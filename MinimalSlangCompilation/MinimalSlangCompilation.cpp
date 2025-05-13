6d640aa637830a7bb96dc1344c170b5872d9d24a
Shannon Woods <shannon@shewritescode.net>
2025-05-13 16:36:10 -0400

    Add explanatory comments

--- MinimalSlangCompilation/MinimalSlangCompilation.cpp
+++ MinimalSlangCompilation/MinimalSlangCompilation.cpp
@@ -6,9 +6,13 @@
 
 int main()
 {
+    // Create the global session - basically, instantiating the compiler & its API
     slang::IGlobalSession* slangGlobalSession;
     slang::createGlobalSession(&slangGlobalSession);
 
+    // Now, we set some attributes to describe how we want the Slang compiler to behave 
+    // when we invoke it. We think of "Uses of Slang with these particular options" as a
+    // (local) session, so we store the options in a SessionDesc
     slang::SessionDesc sessionDesc = {};
 
     // Set our compile target as SPIR-V 1.5
@@ -20,11 +24,15 @@ int main()
     sessionDesc.targets = &targetDesc;
     sessionDesc.targetCount = 1;
 
-    sessionDesc.compilerOptionEntryCount = 0; // using the default compiler options
+    sessionDesc.compilerOptionEntryCount = 0; // otherwise using the default compiler options
 
+    // Now we create the local session, and provide the description that includes target and
+    // option information
     slang::ISession *session;
     slangGlobalSession->createSession(sessionDesc, &session);
 
+    // Lastly, we'll use the compiler session to retrive the slang module, 
+    // providing the source file as input.
     slang::IModule* slangModule = nullptr;
 
     slang::IBlob* diagnosticBlob = nullptr;
