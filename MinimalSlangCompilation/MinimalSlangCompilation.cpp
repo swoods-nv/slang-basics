05a815b76cdbc66f2ee2eeec60e79c5ac48bbf60
Shannon Woods <shannon@shewritescode.net>
2025-05-16 16:28:57 -0400

    Add remaining compile steps to the minimal compile example.

--- MinimalSlangCompilation/MinimalSlangCompilation.cpp
+++ MinimalSlangCompilation/MinimalSlangCompilation.cpp
@@ -6,13 +6,20 @@
 
 int main()
 {
-    // Create the global session - basically, instantiating the compiler & its API
+    // Create the global session - basically, instantiating the compiler & its API,
+    // allowing it to load any downstream libraries or tools needed.
+    // Most projects will do this only once during execution.
     slang::IGlobalSession* slangGlobalSession;
     slang::createGlobalSession(&slangGlobalSession);
 
-    // Now, we set some attributes to describe how we want the Slang compiler to behave 
-    // when we invoke it. We think of "Uses of Slang with these particular options" as a
-    // (local) session, so we store the options in a SessionDesc
+    // We then need to set up a "local session", which is the context for running the 
+    // Slang compiler. Think of this as packaging up all the information you need to provide
+    // to the compiler about what target you want to generate code for, any compiler flags
+    // you want to provide, include library paths, stage and entry point information, etc.
+    // Many projects will end up creating and destroying lots of sessions during execution,
+    // to compile different things for different targets or with different options.
+
+    // We store the options for our local session in a SessionDesc
     slang::SessionDesc sessionDesc = {};
 
     // There are a number of options that can be provided as part of the session description, 
@@ -37,8 +44,9 @@ int main()
     slang::ISession *session;
     slangGlobalSession->createSession(sessionDesc, &session);
 
-    // Lastly, we'll use the compiler session to retrive the slang module, 
-    // providing the source file as input.
+    // Now we'll use the compiler session to retrive the slang module, 
+    // providing the source file as input. This loads the module but does
+    // not perform linking or compilation.
     slang::IModule* slangModule = nullptr;
 
     slang::IBlob* diagnosticBlob = nullptr;
@@ -49,6 +57,45 @@ int main()
         std::cout << "Error loading Slang module" << std::endl;
         return -1;
     }
-    else
-        std::cout << "Loaded Slang module successfully!" << std::endl;
+
+    slang::IEntryPoint* entryPoint;
+
+    std::cout << "Loaded Slang module successfully!" << std::endl;
+
+    // Now we can link our shader program. We're just using a simple 
+    // single-module compute shader -- if we had multiple stages to 
+    // chain together, or our code was spread across multiple modules,
+    // we would need to load them, identify entry points, and compose
+    // the modules into a single composite for linking.
+    SlangResult result;
+    slang::IComponentType* linkedProgram = nullptr;
+    result = slangModule->link(&linkedProgram, &diagnosticBlob);
+    if (SLANG_FAILED(result))
+    {
+        std::cout << "Linking failed!" << std::endl;
+        return -1;
+    }
+    
+    slang::IBlob* spirvCode;
+    diagnosticBlob = nullptr;
+
+    // Now that our module is linked, it's finally time to get the 
+    // resulting target code! This is the step that actually causes
+    // compilation.
+    result = linkedProgram->getTargetCode(
+        0, // targetIndex
+        &spirvCode,
+        &diagnosticBlob);
+
+    if (SLANG_FAILED(result))
+    {
+        std::cout << "Compilation failed!!" << std::endl;
+        return -1;
+    }
+
+    std::cout << "Successfully compiled " << spirvCode->getBufferSize() << " bytes of SPIR-V." << std::endl;
+
+    // Note: This program does not clean up after any of its potential allocations, and
+    // will leak memory. You'll want to be tidier in your own implementation!
+
 }
